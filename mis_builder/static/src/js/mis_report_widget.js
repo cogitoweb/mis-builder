e3ae21ce6e885c0a5b56770d58c9d46bfab46c83
Francesco <fra.guerrazzi@gmail.com>
2022-01-18 14:30:34 +0100

    typo

--- mis_builder/static/src/js/mis_report_widget.js
+++ mis_builder/static/src/js/mis_report_widget.js
@@ -320,7 +320,7 @@ odoo.define("mis_builder.widget", function (require) {
             self.MisReportInstance.call("drilldown", [self._instance_id(), drilldown], {
                 context: context,
             }).then(function (result) {
-                result.drilldown = 50000;
+                result.limit = 50000;
                 self.do_action(result);
             });
         },
