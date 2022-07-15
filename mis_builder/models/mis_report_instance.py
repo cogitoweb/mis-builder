2af246ecbeaa1d477f2a80630b9693768ac83214
2z <paolo.cazzitti@cogitoweb.it>
2022-07-15 11:24:39 +0200

    setup

--- mis_builder/models/mis_report_instance.py
+++ mis_builder/models/mis_report_instance.py
@@ -296,6 +296,27 @@ class MisReportInstancePeriod(models.Model):
             "filters and cannot be modified in the preview."
         ),
     )
+
+    partner_id = fields.Many2one(
+        comodel_name="res.partner",
+        string="Partner",
+        help=(
+            "Filter column on journal entries that match this partner."
+            "This filter is combined with a AND with the report-level filters "
+            "and cannot be modified in the preview."
+        ),
+    )
+
+    account_id = fields.Many2one(
+        comodel_name="account.account",
+        string="Account",
+        help=(
+            "Filter column on journal entries that match this account."
+            "This filter is combined with a AND with the report-level filters "
+            "and cannot be modified in the preview."
+        ),
+    )
+
     analytic_tag_ids = fields.Many2many(
         comodel_name="account.analytic.tag",
         string="Analytic Tags",
@@ -415,6 +436,10 @@ class MisReportInstancePeriod(models.Model):
             domain.extend([("move_id.state", "=", "posted")])
         if self.analytic_account_id:
             domain.append(("analytic_account_id", "=", self.analytic_account_id.id))
+        if self.partner_id:
+            domain.append(("partner_id", "=", self.partner_id.id))
+        if self.account_id:
+            domain.append(("account_id", "=", self.account_id.id))
         if self.analytic_group_id:
             domain.append(
                 ("analytic_account_id.group_id", "=", self.analytic_group_id.id)
@@ -591,10 +616,20 @@ class MisReportInstance(models.Model):
         comodel_name="account.analytic.group",
         string="Analytic Account Group",
     )
+    partner_id = fields.Many2one(
+        comodel_name="res.partner",
+        string="Partner",
+    )
+    account_id = fields.Many2one(
+        comodel_name="account.account",
+        string="Account",
+    )
     analytic_tag_ids = fields.Many2many(
         comodel_name="account.analytic.tag", string="Analytic Tags"
     )
     hide_analytic_filters = fields.Boolean(default=True)
+    hide_partner_filter = fields.Boolean(default=True)
+    hide_account_filter = fields.Boolean(default=True)
 
     @api.onchange("company_id", "multi_company")
     def _onchange_company(self):
@@ -616,14 +651,31 @@ class MisReportInstance(models.Model):
     @api.model
     def get_filter_descriptions_from_context(self):
         filters = self.env.context.get("mis_report_filters", {})
-        analytic_account_id = filters.get("analytic_account_id", {}).get("value")
         filter_descriptions = []
+
+        analytic_account_id = filters.get("analytic_account_id", {}).get("value")
         if analytic_account_id:
             analytic_account = self.env["account.analytic.account"].browse(
                 analytic_account_id
             )
             filter_descriptions.append(
                 _("Analytic Account: %s") % analytic_account.display_name
+            )
+        partner_id = filters.get("partner_id", {}).get("value")
+        if partner_id:
+            partner = self.env["res.partner"].browse(
+                partner_id
+            )
+            filter_descriptions.append(
+                _("Partner: %s") % partner.display_name
+            )
+        account_id = filters.get("account_id", {}).get("value")
+        if account_id:
+            account = self.env["account.account"].browse(
+                account_id
+            )
+            filter_descriptions.append(
+                _("Account: %s") % account.display_name
             )
         analytic_group_id = filters.get("analytic_account_id.group_id", {}).get("value")
         if analytic_group_id:
@@ -719,6 +771,16 @@ class MisReportInstance(models.Model):
         if self.analytic_account_id:
             context["mis_report_filters"]["analytic_account_id"] = {
                 "value": self.analytic_account_id.id,
+                "operator": "=",
+            }
+        if self.partner_id:
+            context["mis_report_filters"]["partner_id"] = {
+                "value": self.partner_id.id,
+                "operator": "=",
+            }
+        if self.account_id:
+            context["mis_report_filters"]["account_id"] = {
+                "value": self.account_id.id,
                 "operator": "=",
             }
         if self.analytic_group_id:
