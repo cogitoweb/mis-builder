0298b40e10a6e2f39b66e1928e98ad0507e4a903
Paolo Cazzitti <paolo.cazzitti@cogitoweb.it>
2026-04-16 10:33:40 +0200

    compat python 3.12

--- mis_builder/models/aep.py
+++ mis_builder/models/aep.py
@@ -2,18 +2,19 @@
 # Copyright 2014 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-import datetime
 import re
-import time
 from collections import defaultdict
-
-import dateutil
 
 from odoo import _, fields
 from odoo.exceptions import UserError
 from odoo.models import expression
 from odoo.tools.float_utils import float_is_zero
-from odoo.tools.safe_eval import safe_eval
+from odoo.tools.safe_eval import (
+    datetime as safe_datetime,
+    dateutil as safe_dateutil,
+    safe_eval,
+    time as safe_time,
+)
 
 from .accounting_none import AccountingNone
 
@@ -141,9 +142,9 @@ class AccountingExpressionProcessor(object):
         domain_eval_context = {
             "ref": self.env.ref,
             "user": self.env.user,
-            "time": time,
-            "datetime": datetime,
-            "dateutil": dateutil,
+            "time": safe_time,
+            "datetime": safe_datetime,
+            "dateutil": safe_dateutil,
         }
         field, mode, account_sel, ml_domain = mo.groups()
         # handle some legacy modes
